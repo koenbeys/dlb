5eee45519da66229a024708922f3b0d88ccf4d4a
Koen Beys <adson@scarlet.be>
2017-01-30 11:34:15 +0100

    postgres config v0.001

--- app/user/views.py
+++ app/user/views.py
@@ -1,68 +1,109 @@
-from flask import redirect, render_template, request,url_for, flash
+from flask import redirect, render_template, request,url_for, flash, get_flashed_messages
 from flask_user import login_required,current_user
 # from flask_login import login_user,abort
 # from .functions import is_safe_url
 from . import user,log
-from .forms import userdetailForm,LoginForm,UserProfileForm, userListForm
+from .forms import userdetailForm,LoginForm,UserProfileForm, userListForm, SetPasswordForm
 from app import app,db
 from . import functions
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     # Here we use a class of some kind to represent and validate our
-#     # client-side form data. For example, WTForms is a library that will
-#     # handle this for us, and we use a custom LoginForm to validate.
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         # Login and validate the user.
-#         # user should be an instance of your `User` class
-#         login_user(user)
-#
-#         flash('Logged in successfully.!!!')
-#
-#         next = request.args.get('next')
-#         # is_safe_url should check if the url is safe for redirects.
-#         # See http://flask.pocoo.org/snippets/62/ for an example.
-#         if not is_safe_url(next):
-#             return abort(400)
-#
-#         return redirect(next or url_for('index'))
-#     return render_template('login.html', form=form)
-
 
 @user.route('/userList', methods=['GET', 'POST'])
 def userList_page():
     log.debug('start def userList_page')
     form = userListForm(request.form)
+    log.debug('form')
     if request.method == 'POST':
+        log.debug("request.method == 'POST'     => True")
         if form.validate():
-            print "Userlistform : ok"
+            log.debug("form.validate()     => True")
             userid1 = request.form.get('userid')
             op = request.form.get('op')
-            print userid1
-            print op
+            log.debug("userid = '%s', op = '%s'",userid1,op)
+
             if (op == "new"):
+                print "new"
                 return redirect(url_for('user.newuser_page'))
             else:
                 if (op == "edit"):
+                    print "user edit"
                     return redirect(url_for('user.userdetail_page', userid=userid1))
+                    # cuser = functions.getUserDetail(userid1)
+                    # croles = functions.getUserRoles(userid1)
+                    #
+                    # return render_template('userdetail.html', cuser=cuser, croles=croles)
+
                 else:
                     print "password update"
                     # should be page for password update
 
-                    return redirect(url_for('user.change_password', userid=userid1))
+                    # return redirect(url_for('user.set_password', userid=userid1))
+                    cuser = functions.getUserDetail(userid1)
+                    forma = SetPasswordForm(request.form)
+                    return render_template('set_passwd.html',form=forma, userid=userid1, cuser=cuser)
 
                     # return redirect(url_for('user.set_password_page', userid=userid1))
         else:
-            print "Userlistform : not ok"
+            log.debug("form.validate()     => False")
             userList = functions.getUserList()
+            log.debug("na userList")
             return render_template( 'userlist.html', tableList=userList, alert=True)
+    else:
+        log.debug("if request.method == 'POST' => False" )
+        userList = functions.getUserList()
+        log.debug("na userList")
 
-    userList = functions.getUserList()
-    return render_template('userList.html', tableList=userList, alert=False)
+        return render_template('userList.html', tableList=userList, alert=False)
 
-# @user.route('/set_password/<userid>', methods=['GET', 'POST'])
-# def change_password():
+
+@user.route('/userdetail/<userid>', methods=['GET', 'POST'])
+def userdetail_page(userid):
+    log.debug('start def userdetail_page(userid)')
+    form = userdetailForm(request.form)
+    print "jjj"
+    print request.form.get('formname')
+    if request.form.get('formname')=='userdetail':
+        if request.method == 'POST':
+            if form.validate():
+                log.debug("userdetailForm : ok")
+                userid = request.form.get('userid')
+                newRoles = request.form.get('newRoles')
+                functions.setUserRoles(int(userid),newRoles)
+                log.debug("roles updated")
+                first_name = request.form.get('first_name')
+                last_name = request.form.get('last_name')
+                email = request.form.get('email')
+                gsmnr = request.form.get('gsmnr')
+                active = request.form.get('active')
+                username = request.form.get('username')
+
+                functions.setUser(int(userid),username,first_name,last_name,email,gsmnr,active)
+                userList = functions.getUserList()
+                # return render_template('userList.html', tableList=userList, alert=False)
+                return redirect(url_for('user.userList_page'))
+
+            else:
+                log.debug("userdetailForm : NOT ok")
+                log.debug("userdetail requets")
+                cuser = functions.getUserDetail(userid)
+                croles = functions.getUserRoles(userid)
+                return render_template('userdetail.html', cuser=cuser, croles = croles, alert=True)
+
+    log.debug("userdetail requets")
+    cuser = functions.getUserDetail(userid)
+    croles = functions.getUserRoles(userid)
+    return render_template('userdetail.html', cuser=cuser, croles = croles, alert=False)
+
+
+
+
+
+@user.route('/set_password/<userid>', methods=['GET', 'POST'])
+def set_password(userid):
+    print userid
+    form = SetPasswordForm(request.form)
+    return render_template('set_passwd.html', form=form)
+
+    app.user_manager
 #     """ Prompt for old password and new password and change the user's password."""
 #     user_manager =  current_app.user_manager
 #     db_adapter = user_manager.db_adapter
@@ -93,7 +134,7 @@ def userList_page():
 #         return redirect(form.next.data)
 #
 #     # Process GET or invalid POST
-#     return render_template(user_manager.change_password_template, form=form)
+
 #
 
 
@@ -134,38 +175,6 @@ def newuser_page():
 
 
 
-@user.route('/userdetail/<userid>', methods=['GET', 'POST'])
-def userdetail_page(userid):
-    log.debug('start def userdetail_page(userid)')
-    form = userdetailForm(request.form)
-    if request.method == 'POST':
-        if form.validate():
-            log.debug("userdetailForm : ok")
-            userid = request.form.get('userid')
-            newRoles = request.form.get('newRoles')
-            functions.setUserRoles(int(userid),newRoles)
-            log.debug("roles updated")
-            first_name = request.form.get('first_name')
-            last_name = request.form.get('last_name')
-            email = request.form.get('email')
-            gsmnr = request.form.get('gsmnr')
-            active = request.form.get('active')
-            username = request.form.get('username')
-
-            functions.setUser(int(userid),username,first_name,last_name,email,gsmnr,active)
-            userList = functions.getUserList()
-            return render_template('userList.html', tableList=userList, alert=False)
-        else:
-            log.debug("userdetailForm : NOT ok")
-            log.debug("userdetail requets")
-            cuser = functions.getUserDetail(userid)
-            croles = functions.getUserRoles(userid)
-            return render_template('userdetail.html', cuser=cuser, croles = croles, alert=True)
-
-    log.debug("userdetail requets")
-    cuser = functions.getUserDetail(userid)
-    croles = functions.getUserRoles(userid)
-    return render_template('userdetail.html', cuser=cuser, croles = croles, alert=False)
 
 
 @user.route('/user/profile', methods=['GET', 'POST'])
